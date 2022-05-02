c6de0419e5532dc84f5c1023848a86a14af6fcbc
Vikrant Reddy <vikrantreddy@hotmail.com>
2022-05-03 02:40:12 +0530

    Generated articles

---
+++ assets/js/articles.js
@@ -0,0 +1,17 @@
+articles = {
+  names: [
+    "Arena",
+    "Bequest",
+    "CareerFoundry",
+    "Digital",
+    "Distilled",
+    "Finny",
+    "GoStudent",
+    "Martech",
+    "Nanonets",
+    "Nicejob",
+    "Paystone",
+    "Zapflow",
+  ],
+  links: [],
+};
